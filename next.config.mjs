353f923b5da8109d2bf5475d7f8692558819cdfd
yungbali <info.prodbybali@gmail.com>
2024-12-09 19:36:59 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,12 @@ const nextConfig = {
   output: 'standalone',
   images: {
     unoptimized: true,
-    domains: ['localhost'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-    ],
   },
   typescript: {
     ignoreBuildErrors: true
   },
   eslint: {
     ignoreDuringBuilds: true
-  },
-  experimental: {
-    serverActions: true
   }
 };
 
