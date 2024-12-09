0f8d0f08b2c2244ce79a6874862b5a73ccdc83da
yungbali <info.prodbybali@gmail.com>
2024-12-09 19:30:02 +0100

    the update

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,11 @@ const nextConfig = {
   reactStrictMode: false,
   eslint: {
     dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    ignoreDuringBuilds: true
   },
   images: {
     unoptimized: true,
+    domains: ['localhost']
   },
   output: 'standalone',
   typescript: {
